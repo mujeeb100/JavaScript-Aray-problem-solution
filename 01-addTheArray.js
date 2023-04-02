bd4aa719611bca8d0a5f635f1e85e6d63cca3c3e
mujeeb ansari <muzeeb100@yahoo.co.in>
2023-04-03 01:41:15 +0530

    1-How do you add an element to the end of an array in JavaScript?

---
+++ 01-addTheArray.js
@@ -0,0 +1,10 @@
+// 1-How do you add an element to the end of an array in JavaScript?
+
+let string1 = ["Mujeeb"];
+let String2 = ["Ansari"];
+// let result = string1[0] + String2[0];
+// concat we used for the converting it into a single array
+let result = string1.concat(String2);
+
+console.log(result);
+
